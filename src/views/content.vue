44dfb586b5bd2613848244b264304894dcccba7f
nuyoahya <purplej5@163.com>
2023-07-22 21:46:56 +0800

    update

---
+++ src/views/content.vue
@@ -0,0 +1,20 @@
+<template>
+
+<div class="content_background">
+
+</div>
+
+</template>
+
+<script setup lang="ts">
+
+</script>
+
+<style scoped>
+.content_background{
+    background-color: aquamarine;
+
+}
+
+</style>
+
